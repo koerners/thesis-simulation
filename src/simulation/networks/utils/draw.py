a85b49ffca628a5eee987a6497dacaaaf044a1bf
Stefan Koerner <git@skoerner.com>
2021-09-27 18:34:22 +0200

    Better graph

--- src/simulation/networks/utils/draw.py
+++ src/simulation/networks/utils/draw.py
@@ -6,8 +6,8 @@ from simulation.utils.directory import create_dir
 
 def save_network_visualization(self, out: str) -> None:
     full_path = create_dir(out)
-    plt.figure(figsize=(10, 6))
-    pos = nx.spring_layout(self.graph, k=0.8)
-    nx.draw(self.graph, pos, with_labels=False, width=0.4,
-            node_color='lightblue', node_size=400)
+    plt.figure(figsize=(20, 12))
+    pos = nx.planar_layout(self.graph, center=[0,0])
+    nx.draw(self.graph, pos, with_labels=False, width=0.05,
+            node_color='blue', node_size=2)
     plt.savefig(full_path)
