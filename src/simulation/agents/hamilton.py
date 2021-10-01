25dc7dd1135346fdaf82f42e201447ba049ed406
Stefan Koerner <git@skoerner.com>
2021-10-01 15:30:06 +0200

    fix hamilton not eating

--- src/simulation/agents/hamilton.py
+++ src/simulation/agents/hamilton.py
@@ -7,4 +7,5 @@ class HamiltonAgent(EatingAgent):
         super().__init__(model=model, age=age)
 
     def bear_child(self):
+        self.current_food -= self.model.child_bearing_cost
         return HamiltonAgent(self.model, age=0)
