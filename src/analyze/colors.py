a37454e06af84e6342c6f7c2d9593fc177ee46fb
Stefan Koerner <git@skoerner.com>
2021-12-30 14:07:00 +0100

    move colors to new file

---
+++ src/analyze/colors.py
@@ -0,0 +1,42 @@
+import random
+
+from palettable.cartocolors.qualitative import Prism_10
+from palettable.cartocolors.qualitative import Vivid_10
+# https://jiffyclub.github.io/palettable/
+color_pallet = Prism_10.mpl_colors
+
+COLOR_MAP = {
+    # Agents
+    "GroupAgent": color_pallet[1],
+    "CultureAgent": color_pallet[2],
+    "AltruismAgent": color_pallet[3],
+    "UnconditionalAgent": color_pallet[4],
+    "FakeGreenBeardAgent": color_pallet[5],
+    "GreenbeardAgent": color_pallet[6],
+    "ReputationAgent": color_pallet[7],
+    "EatingAgent": color_pallet[8],
+    "KinSelectionAgent": color_pallet[9],
+    # Statistics
+    "below_median": color_pallet[2],
+    "above_median": color_pallet[-1],
+    "average": color_pallet[5],
+    "avg_fitness_alt": color_pallet[2],
+    "avg_fitness_non_alt": color_pallet[-1],
+    # Numbers
+    "0": color_pallet[0],
+    "1": color_pallet[1],
+    "2": color_pallet[2],
+    "3": color_pallet[3],
+    "4": color_pallet[4],
+    "5": color_pallet[6],
+    "6": color_pallet[7],
+    "7": color_pallet[8],
+    "8": color_pallet[9],
+    # Boolean
+    "True": color_pallet[2],
+    "False": color_pallet[-1],
+}
+
+
+def get_color_by_value(value):
+    return COLOR_MAP.get(str(value), random.choice(Vivid_10.mpl_colors))
