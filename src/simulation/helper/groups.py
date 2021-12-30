09b31575a28e1dd23b1a586d03a7d1df4d141f93
Stefan Koerner <git@skoerner.com>
2021-12-30 19:14:51 +0100

    culture group name

--- src/simulation/helper/groups.py
+++ src/simulation/helper/groups.py
@@ -7,7 +7,7 @@ class Group:
 class CultureGroup(Group):
     def __init__(self, group_id, group_culture):
         self.group_culture = group_culture  # val between 0 and 1
-        super().__init__(group_id=group_id)  # A, B, C ...
+        super().__init__(group_id=f"{group_id}-{group_culture}")  # A, B, C ...
 
     def agent_acted_altruistic(self):
         self.group_culture = min(self.group_culture + 0.0001, 1)
