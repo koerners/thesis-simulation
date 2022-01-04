e9039071de45d9a18a677cd025259eb631361403
koerners <git@skoerner.com>
2022-01-04 11:39:41 +0000

    [Automated] format

--- src/tests/test_models.py
+++ src/tests/test_models.py
@@ -29,14 +29,12 @@ class ModelsTest(unittest.TestCase):
 
     def assert_test_group(self, model):
         """assert there are two types of agents present in the model: unconditional and eating"""
-        existing_types_of_agents = set(
-            agent.__class__ for agent in model.agents)
+        existing_types_of_agents = set(agent.__class__ for agent in model.agents)
         self.assertIn(UnconditionalAgent, existing_types_of_agents)
         self.assertIn(EatingAgent, existing_types_of_agents)
 
     def test_base(self):
-        model = BaseModel(
-            num_agents=10, network_saving_steps=None, run_id=None)
+        model = BaseModel(num_agents=10, network_saving_steps=None, run_id=None)
         self.assertIsInstance(model, BaseModel)
         self.assertEqual(model.schedule.get_agent_count(), 10)
         self.assert_step(model)
