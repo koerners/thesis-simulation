a7ceb1deac530bd77b839a24476600a17d6a67d5
Stefan Koerner <git@skoerner.com>
2022-01-16 14:36:44 +0100

    fix group culture

--- src/simulation/agents/culture.py
+++ src/simulation/agents/culture.py
@@ -7,15 +7,17 @@ class CultureAgent(GroupAgent):
         super().__init__(model=model, group=group, age=age)
 
     def find_peer_in_need(self) -> Agent:
-        willing_to_help = (
-            self.random.uniform(0, 1)
-            <= self.model.get_group_of_agent(self).group_culture
-        )
-        if willing_to_help:
-            for peer in self.model.agents:
-                if peer != self and peer.current_food < 1 and peer.group == self.group:
-                    self.model.agent_acted_altruistic(self)
-                    return peer
+        group = self.model.get_group_of_agent(self)
+        if group is not None:
+            willing_to_help = (
+                self.random.uniform(0, 1)
+                <= group.group_culture
+            )
+            if willing_to_help:
+                for peer in self.model.agents:
+                    if peer != self and peer.current_food < 1 and peer.group == self.group:
+                        self.model.agent_acted_altruistic(self)
+                        return peer
         else:
             self.model.agent_acted_non_altruistic(self)
         return None
